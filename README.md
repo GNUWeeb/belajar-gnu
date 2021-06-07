fb9096a8208794640606b8f1bcc5fedf69e0c880
Ammar Faizi <ammarfaizi2@gmail.com>
2021-06-07 17:24:26 +0700

    Add indexes
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Belajar GNU
 Materi belajar GNU di grup Telegram (https://t.me/GNUWeeb)
+
+# License
+GNU GPL 2.0
---
+++ gen_index.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: GPL-2.0
+#
+# Copyright (C) 2021  Ammar Faizi <ammarfaizi2@gmail.com>
+#
+
+ln -svf ../../materi/001 indexes/gnu_c/001
